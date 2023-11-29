94f884e23f391c00c8ae2dfa45e2766128f1a902
HemSunder <pasupuletihemsundar09@gmail.com>
2023-11-29 14:07:10 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Container() {
+  return (
+    <html>
+      <h1>Hello World!</h1>
+      <p>This is a sample Code
+      </p>
+    </html>
+  );
+}
+export default Container;
